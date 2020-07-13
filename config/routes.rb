310218412e8932b7ffb8790af6094102b998c224
Syed AbuTalib <syedabutalib@github.com>
2020-07-13 16:15:13 -0500

    added redirection with a shorter url

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :links
 
   root 'links#new'
-  get ':short',       to: redirect(path: 'links/1')
+  get ':short',       to: redirect { |params, request|
+    path = Link.find_by(short_link: params[:short])
+    "http://#{request.host_with_port}/links/#{path.id}"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
