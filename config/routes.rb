45acc3c1d70cf1db18676f91dc0c1e523542878a
Syed AbuTalib <syedabutalib@github.com>
2020-07-13 16:39:53 -0500

    added case of unexisting short url

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   root 'links#new'
   get ':short',       to: redirect { |params, request|
     path = Link.find_by(short_link: params[:short])
-    "http://#{request.host_with_port}/links/#{path.id}"
+    if path.nil?
+      'links/new'
+    else
+      "http://#{request.host_with_port}/links/#{path.id}"
+    end
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
