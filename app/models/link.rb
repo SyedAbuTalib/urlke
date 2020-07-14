87a8227eaf36570a52b742897106ac8ac93ceea6
Syed AbuTalib <syedabutalib@github.com>
2020-07-14 11:54:53 -0500

    added validations for shortlink and fulllink

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Link < ApplicationRecord
+  validates :full_link, presence: true
+  validates :short_link, presence: true
+  validates_format_of :short_link, with: /\A[\w]+\z/, on: :create, message: "must be alphanumeric and underscores only"
 end
