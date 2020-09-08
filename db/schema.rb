19e5723acba7dce9e4c22c0d6fd759145c226b8f
Syed AbuTalib <syedabutalib@github.com>
2020-09-08 16:17:26 -0500

    changed from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_14_175819) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.string "short_link"
     t.string "full_link"
